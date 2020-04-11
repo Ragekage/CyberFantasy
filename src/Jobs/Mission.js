ebf7ec5e6c94ecbf28cf761d2f9effd7b6aa874b
Kade <kadedavies@hotmail.co.uk>
2020-04-11 22:49:23 +0100

    Added Jobs Folder with Mission.js
    added Jobs folder for future mission code.

---
+++ src/Jobs/Mission.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Mission extends Component {
+
+
+
+
+    render()
+    {
+        return(
+            <div>
+                Mission
+            </div>
+        )
+    }
+}
+
+export default Mission
