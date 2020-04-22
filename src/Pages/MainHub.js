9b244d9c00662d1632339013332179fb50552ba0
Kade <kadedavies@hotmail.co.uk>
2020-04-22 16:50:18 +0100

    backend test

--- src/Pages/MainHub.js
+++ src/Pages/MainHub.js
@@ -28,10 +28,28 @@ class MainHub extends Component {
 
         player: [],
         givenPoints: 10,
+        data: null,
     }
     this.playerRef = React.createRef()
 
     }
+
+    componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
 
     render(){
         return(
